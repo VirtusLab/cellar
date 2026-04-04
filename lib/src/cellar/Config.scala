b794a3c70318227860f77759e70c686200ebec83
Yannick Heiber <yhe@famly.co>
2026-04-04 18:51:10 +0200

    Introduce config that is loaded from file
    To allow users to conveniently configure certain aspects of cellar, introduce an optional config file. For starters, this allows configuring the mill and sbt binaries as well as the sbt invocation mode. Ships with a built-in default config that does not alter the current behavior.

---
+++ lib/src/cellar/Config.scala
@@ -0,0 +1,32 @@
+package cellar
+
+import cats.effect.IO
+import pureconfig.*
+
+import java.nio.file.{Files, Path}
+
+case class MillConfig(binary: String) derives ConfigReader
+
+case class SbtConfig(useClientMode: Boolean, binary: String) derives ConfigReader
+
+case class Config(mill: MillConfig, sbt: SbtConfig) derives ConfigReader
+
+object Config {
+  val default: IO[Config] = load(None)
+
+  val defaultPath: Path = Path.of(".cellar").resolve("cellar.conf")
+
+  def load(path: Option[Path]): IO[Config] = {
+    def load(path: Option[Path]) =
+      IO.blocking {
+        path.foldLeft(ConfigSource.default)((cs, p) => ConfigSource.file(p).withFallback(cs)).loadOrThrow[Config]
+      }
+
+    path match
+      case sp: Some[_] => load(sp)
+      case None => IO.blocking(Files.exists(defaultPath)).flatMap {
+        case true  => load(Some(defaultPath))
+        case false => load(None)
+      }
+  }
+}
